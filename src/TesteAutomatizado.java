c37b18fe132623437012347e0d9c4cc9cc0c2fd7
Aroldo <aroldoxleite@gmail.com>
2017-06-24 06:19:55 -0300

    exemplo inicial

---
+++ src/TesteAutomatizado.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteAutomatizado {
+	public static void main(String[] args) {
+		
+		//Abaixo site para baixar drivers do chrome, atenção com a versão do navegador ser compatível com a do site
+		//https://github.com/mozilla/geckodriver/releases
+		//System.setProperty("webdriver.gecko.driver", "/home/aroldo/Projetos/testes-de-sistema/geckodriver");
+		
+		//Abaixo site para baixar drivers do chrome, atenção com a versão do navegador ser compatível com a do site
+		//https://sites.google.com/a/chromium.org/chromedriver/downloads
+		System.setProperty("webdriver.chrome.driver", "/home/aroldo/Projetos/testes-de-sistema/drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com.br/");
+		
+		WebElement campoBusca = driver.findElement(By.name("q"));
+		campoBusca.sendKeys("Caelum");	
+		campoBusca.submit();
+	}
+}
